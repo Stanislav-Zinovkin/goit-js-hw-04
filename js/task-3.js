7d4e61d25a3649c4df4b2b91f2d4ec786a38a02c
Stanislav-Zinovkin <stas14zinovkin@gmail.com>
2025-01-24 11:05:52 +0200

    Hw#4-js-v0.1

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,18 @@
-function filterArray(numbers, value) {
-  const newArray = [];
-  for (let i = 0; i < numbers.length; i += 1) {
-    if (numbers[i] > value) {
-      newArray.push(numbers[i]);
-    }
-  }
-  return newArray;
-}
+const profile = {
+  username: "Jacob",
+  playTime: 300,
 
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([1, 2, 3, 4, 5], 4));
-console.log(filterArray([1, 2, 3, 4, 5], 5));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+  changeUsername(newName) {
+    this.username = newName;
+  },
+
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+
+  getInfo() {
+    return `${this.username} has ${this.playTime} aactive hours!`;
+  },
+};
+
+console.log(profile.getInfo());
